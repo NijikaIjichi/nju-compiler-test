df19a632eddb37588fdea7e3a6186224c97bfdaf
NijikaIjichi <19187263+NijikaIjichi@users.noreply.github.com>
2024-04-14 16:07:04 +0800

    can report false positive

--- lab1/test.py
+++ lab1/test.py
@@ -10,8 +10,7 @@ wait = False
 timeout = 1.0
 more = False
 color_option = 'auto'
-total = 0
-passed = 0
+result = {'AC': 0, "FalsePositive": 0, 'WA': 0, 'RE': 0, 'TLE': 0, 'total': 0}
 
 
 def main():
@@ -39,7 +38,7 @@ def main():
       test_dir(os.path.join('extend', dir))
   if advance:
     test_advance()
-  print(f'PASSED {passed}/{total}')
+  print(', '.join([f'{i}: {j}' for i, j in result.items() if j > 0]))
 
 
 def getsub(file: str):
@@ -57,7 +56,7 @@ def pause():
       exit()
 
 
-COLORS = {"default": "\033[0m", "red": "\033[31m", "green": "\033[32m"}
+COLORS = {"default": "\033[0m", "red": "\033[31m", "green": "\033[32m", "yellow": "\033[33m"}
 
 
 def color(name: str, text: str):
@@ -98,46 +97,48 @@ def run_one(file: str):
     p = run([parser, file], stdin=PIPE, stdout=PIPE, stderr=PIPE, text=True, encoding="utf-8", timeout=timeout)
   except TimeoutExpired:
     print(f"[{file}] {color('red', 'TLE')}")
-    return False
+    return 'TLE'
   if p.returncode != 0:
     print(f"[{file}] {color('red', 'RE')}")
-    return False
+    return 'RE'
   return p
 
 
 def test_one(file: str):
   p = run_one(file)
-  if not p:
-    return False
+  if isinstance(p, str):
+    return p
   ans = get_ans(file)
   if ans:
     err = analyse_out(p.stdout)
     if err in ans:
       print(f"[{file}] {color('green', 'AC')}")
-      return True
+      return 'AC'
+    elif (nearest := max([i for i in ans if set(i).issubset(err)] + [[]], key=len)) != []:
+      print(f"[{file}] {color('yellow', 'FalsePositive')}: got {err}, should NOT report {[i for i in err if i not in nearest]}")
+      return 'FalsePositive'
     else:
       print(f"[{file}] {color('red', 'WA')}: got {err}, expect {ans[0]}")
-      return False
+      return 'WA'
   else:
     with open(chsub(file, 'out')) as fp:
       ans = fp.read()
     if p.stdout.strip() == ans.strip():
       print(f"[{file}] {color('green', 'AC')}")
-      return True
+      return 'AC'
     else:
-      print(f"[{file}] {color('red', 'WA')}")
-      return False
+      print(f"[{file}] {color('red', 'WA')}: wrong grammar tree")
+      return 'WA'
 
 
 def test_dir(dir: str):
-  global total, passed
   for path, _, files in os.walk(dir):
     for file in sorted(filter(lambda x: getsub(x) == 'cmm', files)):
       file = os.path.join(path, file)
-      total += 1
-      if test_one(file):
-        passed += 1
-      else:
+      result['total'] += 1
+      r = test_one(file)
+      result[r] += 1
+      if r != 'AC':
         pause()
 
 
@@ -180,41 +181,45 @@ def compare(output, ans):
 
 def test_advance_one(file: str):
   p = run_one(file)
-  if not p:
-    return False
+  if isinstance(p, str):
+    return p
   with open(chsub(file, 'output')) as fp:
     ans = fp.read()
-  if anserr := analyse_out(ans, False):
-    outerr = analyse_out(p.stdout, False)
+  if anserr := sorted(analyse_out(ans, False)):
+    outerr = sorted(analyse_out(p.stdout, False))
     if more:
-      ac = all(i in outerr for i in anserr)
+      ac = set(anserr).issubset(outerr)
+      false_positive = False
     else:
-      ac = anserr == outerr
+      ac = set(anserr) == set(outerr)
+      false_positive = set(anserr).issubset(outerr)
     if ac:
       print(f"[{file}] {color('green', 'AC')}")
-      return True
+      return 'AC'
+    elif false_positive:
+      print(f"[{file}] {color('yellow', 'FalsePositive')}: got {outerr}, should NOT report {[i for i in outerr if i not in anserr]}")
+      return 'FalsePositive'
     else:
-      print(f"[{file}] {color('red', 'WA')}: got {sorted(outerr)}, expect {sorted(anserr)}")
-      return False
+      print(f"[{file}] {color('red', 'WA')}: got {outerr}, expect {anserr}")
+      return 'WA'
   else:
     i, o, a = compare(p.stdout.strip().splitlines(), clean_ans(ans))
     if i == 0:
       print(f"[{file}] {color('green', 'AC')}")
-      return True
+      return 'AC'
     else:
       print(f"[{file}] {color('red', 'WA')}: err @ Line{i}, got {o}, expect {a}")
-      return False
+      return 'WA'
 
 
 def test_advance():
-  global total, passed
   for path, _, files in os.walk('advance'):
     for file in sorted(filter(lambda x: getsub(x) == 'cmm', files)):
       file = os.path.join(path, file)
-      total += 1
-      if test_advance_one(file):
-        passed += 1
-      else:
+      result['total'] += 1
+      r = test_advance_one(file)
+      result[r] += 1
+      if r != 'AC':
         pause()
 
 
--- lab2/test.py
+++ lab2/test.py
@@ -9,8 +9,7 @@ wait = False
 timeout = 1.0
 more = False
 color_option = 'auto'
-total = 0
-passed = 0
+result = {'AC': 0, "FalsePositive": 0, 'WA': 0, 'RE': 0, 'TLE': 0, 'total': 0}
 
 
 def main():
@@ -38,7 +37,7 @@ def main():
       test_dir(os.path.join('extend', dir))
   if advance:
     test_advance()
-  print(f'PASSED {passed}/{total}')
+  print(', '.join([f'{i}: {j}' for i, j in result.items() if j > 0]))
 
 
 def getsub(file: str):
@@ -56,7 +55,7 @@ def pause():
       exit()
 
 
-COLORS = {"default": "\033[0m", "red": "\033[31m", "green": "\033[32m"}
+COLORS = {"default": "\033[0m", "red": "\033[31m", "green": "\033[32m", "yellow": "\033[33m"}
 
 
 def color(name: str, text: str):
@@ -92,36 +91,38 @@ def run_one(file: str):
     p = run([parser, file], stdin=PIPE, stdout=PIPE, stderr=PIPE, text=True, encoding="utf-8", timeout=timeout)
   except TimeoutExpired:
     print(f"[{file}] {color('red', 'TLE')}")
-    return False
+    return 'TLE'
   if p.returncode != 0:
     print(f"[{file}] {color('red', 'RE')}")
-    return False
+    return 'RE'
   return p
 
 
 def test_one(file: str):
   p = run_one(file)
-  if not p:
-    return False
+  if isinstance(p, str):
+    return p
   ans = get_ans(file)
   err = analyse_out(p.stdout)
   if err in ans:
     print(f"[{file}] {color('green', 'AC')}")
-    return True
+    return 'AC'
+  elif (nearest := max([i for i in ans if set(i).issubset(err)] + [[]], key=len)) != []:
+    print(f"[{file}] {color('yellow', 'FalsePositive')}: got {err}, should NOT report {[i for i in err if i not in nearest]}")
+    return 'FalsePositive'
   else:
     print(f"[{file}] {color('red', 'WA')}: got {err}, expect {ans[0]}")
-    return False
+    return 'WA'
 
 
 def test_dir(dir: str):
-  global total, passed
   for path, _, files in os.walk(dir):
     for file in sorted(filter(lambda x: getsub(x) == 'cmm', files)):
       file = os.path.join(path, file)
-      total += 1
-      if test_one(file):
-        passed += 1
-      else:
+      result['total'] += 1
+      r = test_one(file)
+      result[r] += 1
+      if r != 'AC':
         pause()
 
 
@@ -131,29 +132,33 @@ def test_advance_one(file: str):
     return False
   with open(chsub(file, 'output')) as fp:
     ans = fp.read()
-  anserr = analyse_out(ans)
-  outerr = analyse_out(p.stdout)
+  anserr = sorted(analyse_out(ans))
+  outerr = sorted(analyse_out(p.stdout))
   if more:
-    ac = all(i in outerr for i in anserr)
+    ac = set(anserr).issubset(outerr)
+    false_positive = False
   else:
-    ac = anserr == outerr
+    ac = set(anserr) == set(outerr)
+    false_positive = set(anserr).issubset(outerr)
   if ac:
     print(f"[{file}] {color('green', 'AC')}")
-    return True
+    return 'AC'
+  elif false_positive:
+    print(f"[{file}] {color('yellow', 'FalsePositive')}: got {outerr}, should NOT report {[i for i in outerr if i not in anserr]}")
+    return 'FalsePositive'
   else:
-    print(f"[{file}] {color('red', 'WA')}: got {sorted(outerr)}, expect {sorted(anserr)}")
-    return False
+    print(f"[{file}] {color('red', 'WA')}: got {outerr}, expect {anserr}")
+    return 'WA'
 
 
 def test_advance():
-  global total, passed
   for path, _, files in os.walk('advance'):
     for file in sorted(filter(lambda x: getsub(x) == 'cmm', files)):
       file = os.path.join(path, file)
-      total += 1
-      if test_advance_one(file):
-        passed += 1
-      else:
+      result['total'] += 1
+      r = test_advance_one(file)
+      result[r] += 1
+      if r != 'AC':
         pause()
 
 
